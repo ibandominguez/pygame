04eb6c0a48c666cd70060bb8c59e5a6d6a498b4c
ibandominguez <domingueznoda@gmail.com>
2019-10-02 19:06:19 +0100

    bike sprite

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import constants
 from sprites.player import Player
+from sprites.sprite import Sprite
 
 pygame.init()
 pygame.display.set_caption(constants.TITLE)
@@ -8,15 +9,41 @@ screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT)) # pygame.d
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-player = Player(width=135, height=115, x=200, y=400, frames_count=25)
-all_sprites.add(player)
 
+player = Player(
+    width=135,
+    height=115,
+    x=100,
+    y=400,
+    frames_count=25
+)
+
+bike = Sprite(
+    file_path='assets/bike.png',
+    width=135,
+    height=115,
+    x=200,
+    y=400,
+    frames_count=25
+)
+
+all_sprites.add(player)
+all_sprites.add(bike)
+
+rpm = 0
 
 while True:
     clock.tick(constants.FPS)
+
+    if rpm > 300: rpm = 0
+    else: rpm += 0.1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+    # RPM to define
+    bike.set_animation_speed(rpm)
 
     all_sprites.update()
     screen.fill(constants.WHITE)
--- sprites/sprite.py
+++ sprites/sprite.py
@@ -2,11 +2,11 @@ import pygame
 import time
 
 
-class Player(pygame.sprite.Sprite):
+class Sprite(pygame.sprite.Sprite):
 
-    def __init__(self, width, height, x, y, frames_count):
+    def __init__(self, file_path, width, height, x, y, frames_count):
         pygame.sprite.Sprite.__init__(self)
-        self.sheet = pygame.image.load('assets/bike.png').convert_alpha()
+        self.sheet = pygame.image.load(file_path).convert_alpha()
         self.image = pygame.transform.scale(self.sheet, (width, height))
         self.rect = self.image.get_rect()
         self.rect.centerx = x  # center of rectangle
@@ -16,6 +16,9 @@ class Player(pygame.sprite.Sprite):
         self.total_frames = frames_count
         self.frames_per_second = 300
         self.setup_animations(width, height)
+
+    def set_animation_speed(self, speed):
+        self.frames_per_second = speed
 
     def setup_animations(self, width, height):
         for i in range(self.total_frames):
