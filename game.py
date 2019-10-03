33de36318a5f2083ed499375e88ba1772020f4cb
ibandominguez <domingueznoda@gmail.com>
2019-10-03 05:23:00 +0100

    all sprites

--- game.py
+++ game.py
@@ -6,14 +6,16 @@ from sprites.sprite import Sprite
 pygame.init()
 pygame.display.set_caption(constants.TITLE)
 screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT)) # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen.fill(constants.BLACK)
 clock = pygame.time.Clock()
 rpm = 0
 
-bike = Sprite( file_path='assets/bike.png', width=135, height=115, x=200, y=400, frames_count=25)
-donuts = Sprite( file_path='assets/donuts.png', width=135, height=240, x=200, y=400, frames_count=50).set_animation_speed(35)
+road = Sprite(file_path='assets/road.png', width=270, height=480, x=200, y=400, frames_count=35).set_animation_speed(35)
+bike = Sprite(file_path='assets/bike.png', width=135, height=115, x=200, y=400, frames_count=25)
+donuts = Sprite(file_path='assets/donuts.png', width=135, height=240, x=200, y=400, frames_count=50).set_animation_speed(35)
 
 sprites = pygame.sprite.Group()
-sprites.add(bike, donuts)
+sprites.add(road, bike, donuts)
 
 while True:
     clock.tick(constants.FPS)
@@ -21,14 +23,17 @@ while True:
     if rpm > 300: rpm = 0
     else: rpm += 0.1
 
+    print(rpm)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
     # RPM to define
-    bike.set_animation_speed(rpm)
+    road.set_animation_speed(int(rpm))
+    bike.set_animation_speed(int(rpm))
 
     sprites.update()
-    screen.fill(constants.WHITE)
+    screen.fill(constants.BLACK)
     sprites.draw(screen)
     pygame.display.flip()
