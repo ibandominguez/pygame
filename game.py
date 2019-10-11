1db9c5a1b90baf5dbb382108aa9208e3bc8e03c5
ibandominguez <domingueznoda@gmail.com>
2019-10-11 06:42:46 +0100

    cleanup comments

--- game.py
+++ game.py
@@ -69,10 +69,6 @@ except Exception as e:
     print('RPI module not found, Sensor not initialized')
 
 """ Sprite Sheets """
-# intro = Sprite(file_path=os.getcwd() + '/assets/intro.png', width=constants.WIDTH, height=constants.HEIGHT, x=0, y=0, frames_tile=(6, 12), frames_total=72).set_animation_speed(60)
-# road = Sprite(file_path=os.getcwd() + '/assets/road.png', width=constants.WIDTH, height=constants.HEIGHT, x=0, y=0, frames_tile=(17, 2), frames_total=34)
-# bike = Sprite(file_path=os.getcwd() + '/assets/bike.png', width=constants.WIDTH, height=constants.HEIGHT, x=0, y=0, frames_tile=(5, 10), frames_total=50)
-# donuts = Sprite(file_path=os.getcwd() + '/assets/donuts.png', width=constants.WIDTH, height=constants.HEIGHT, x=0, y=0, frames_tile=(5, 10), frames_total=50)
 intro = FrameSprite(directory=os.getcwd() + '/pre-assets/intro', width=constants.WIDTH, height=constants.HEIGHT, total_frames=72).set_animation_speed(35)
 road = FrameSprite(directory=os.getcwd() + '/pre-assets/road', width=constants.WIDTH, height=constants.HEIGHT, total_frames=34)
 bike = FrameSprite(directory=os.getcwd() + '/pre-assets/bike', width=constants.WIDTH, height=constants.HEIGHT, total_frames=50)
